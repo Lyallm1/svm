aa8219e08f7da3224c40c719d066e9b1db11aac0
Lyallm1 <81273053+Lyallm1@users.noreply.github.com>
2023-06-08 14:47:48 +0800

    .

--- example/iris.js
+++ example/iris.js
@@ -1,33 +1,7 @@
-'use strict';
+import { getClasses, getNumbers } from 'ml-dataset-iris';
 
-const iris = require('ml-dataset-iris');
-const CV = require('ml-cross-validation');
-const SVM = require('..');
+import SVM from '..';
+import { leaveOneOut } from 'ml-cross-validation';
 
-
-var labels = iris.getClasses();
-const features = iris.getNumbers();
-
-// Separate in 2 categories
-labels = labels.map(label => {
-    if(label === labels[0]) return 1;
-    return -1;
-});
-//
-// const features = [[20, 10],[30,30]];
-// var labels = [-1, 1];
-
-// var svm = new SVM();
-// svm.train(features, labels);
-
-console.time('cv');
-const result = CV.leaveOneOut(SVM, features, labels, {
-    kernel: 'rbf',
-    C: 1,
-    kernelOptions: {
-        sigma: 0.2,
-    }
-});
-console.timeEnd('cv');
-console.log(result);
-
+const labels = getClasses();
+console.log(leaveOneOut(SVM, getNumbers(), labels.map(label => label == labels[0] ? 1 : -1), { kernel: 'rbf', C: 1, kernelOptions: { sigma: 0.2 } }));
